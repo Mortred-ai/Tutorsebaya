4452d77a289190f05a901016a8a73eadeaa0dda1
Bakti <baktiarmahardika08@gmail.com>
2021-07-12 12:01:30 +0700

    Update bhs.png, bio.png, and 9 more files...

--- js/dist/js.js
+++ js/dist/js.js
@@ -10,12 +10,21 @@ wow.init();
 $('.slider').slick({
   dots: true,
   infinite: false,
-  // speed: 100,
-  slidesToShow: 6,
+  speed: 350,
+  slidesToShow: 7,
   slidesToScroll: 1,
   responsive: [
     {
       breakpoint: 1024,
+      settings: {
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 768,
       settings: {
         slidesToShow: 5,
         slidesToScroll: 1,
@@ -27,14 +36,16 @@ $('.slider').slick({
       breakpoint: 600,
       settings: {
         slidesToShow: 4,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        dots: false
       }
     },
     {
       breakpoint: 480,
       settings: {
         slidesToShow: 3,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        dots: false
       }
     }
     // You can unslick at a given breakpoint now by adding:
